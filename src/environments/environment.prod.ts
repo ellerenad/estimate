fa428acecf4e9404142b643273118bad47a79582
Enrique Llerena Dominguez <ellerenad@hotmail.com>
2022-06-18 21:04:43 +0200

    deploy: add env prod ts, increase size budget

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyA7CB7tuW8m_dFLMfMNRSMsLM8A_DjgYJc',
+    authDomain: 'weestimate.firebaseapp.com',
+    projectId: 'weestimate',
+    storageBucket: 'weestimate.appspot.com',
+    messagingSenderId: '550030602319',
+    appId: '1:550030602319:web:0aa47aaae66d4b2c043a3a'
+  }
+
 };
